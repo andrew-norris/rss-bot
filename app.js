4c0908b4c9c66ea8bae1eb78e1b09d359e7054e5
andrewnorris97 <anorris97@gmail.com>
2019-07-10 19:42:59 -0400

    AN - add firebase environment variables.

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const { port, token, signingSecret } = require('./config');
+const { 
+    port, 
+    slackToken, 
+    slackSigningSecret,
+    firebaseApiKey,
+    firebaseAuthDomain,
+    firebaseDatabaseUrl,
+    firebaseProjectId,
+    firebaseMessagingSenderId,
+    firebaseAppId
+} = require('./config');
 const { App } = require('@slack/bolt');
 var firebase = require('firebase/app');
 require('firebase/firestore');
 
+var firebaseConfig = {
+    apiKey: firebaseApiKey,
+    authDomain: firebaseAuthDomain,
+    databaseURL: firebaseDatabaseUrl,
+    projectId: firebaseProjectId,
+    storageBucket: "",
+    messagingSenderId: firebaseMessagingSenderId,
+    appId: firebaseAppId
+  };
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-    token: token,
-    signingSecret: signingSecret
+    token: slackToken,
+    signingSecret: slackSigningSecret
 });
 
 (async () => {
