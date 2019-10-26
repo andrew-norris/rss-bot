d9ca2f9c5c3d918746ecc44e39bcdaaa3a4fe795
andrew norris <anorris97@gmail.com>
2019-10-26 12:24:15 -0400

    AN - initialize admin app and firestore.

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,10 @@
 const functions = require('firebase-functions');
 const request = require('request');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//     response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const firestore = admin.firestore();
+
 
 exports.redirect = functions.https.onRequest((req, res) => {
     console.log(req)
