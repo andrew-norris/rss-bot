61c8b02d3d290849639276a2c8820f21a48829ab
andrew norris <anorris97@gmail.com>
2019-10-25 21:54:00 -0400

    AN - create redirect function to generate redirect url for slack.

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//     response.send("Hello from Firebase!");
 // });
 
+exports.redirect = functions.https.onRequest((request, response) => {
+    response.send("Hello");
+}); 
